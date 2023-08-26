eb0e7b09b056e4a3f47f761b8c761b1a60271a26
JoJoJux <80523427+J0J0HA@users.noreply.github.com>
2023-08-27 01:45:02 +0200

    Added Chrome title fallback - Also added module page replacement

--- views/terminal/script.js
+++ views/terminal/script.js
@@ -1,29 +1,52 @@
 const page = (window.location.hash.substring(1) || "/");
 
+let platform = "unknown";
+
+let trusted_resource = false;
+
 if (typeof browser == "undefined" && typeof chrome != "undefined") {
     var browser = chrome;
+    platform = "chrome";
 };
 
 window.onmessage = (event) => {
-    console.log(event.data)
-    if (event.data.open) {
+    if (event.data.init) {
+        trusted_resource = true;
+    }
+    else if (event.data.open) {
         window.location.href = event.data.open;
     }
     else if (event.data.set_title) {
-        document.querySelector("title").innerText = event.data.set_title;
+        if (platform == "chrome" && event.data.no_content) {
+            document.querySelector("title").innerText = "> " + event.data.set_title;
+        } else {
+            document.querySelector("title").innerText = event.data.set_title;
+        }
     }
 };
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    (async () => {
-        const permissions = await browser.permissions.getAll();
-        if (permissions.origins.indexOf("*://gterminal.is-a.dev/*")) {
-            window.location.href = browser.runtime.getURL("/views/install/index.html");
+
+    if (page.startsWith("/modules/")) {
+        document.body.innerText = "In the future, here should be an install helper. For now, I can only say:\n";
+        if (page.endsWith(".js")) {
+            document.body.innerText += "This is a module resource.";
+        } else if (page.endsWith(".json")) {
+            document.body.innerText += "This is a repo resource.";
         } else {
-            const iframe = document.createElement("iframe");
-            iframe.src = "https://gterminal.is-a.dev" + page;
-            document.body.appendChild(iframe);
-        };
-    })();
+            document.body.innerText += "This is an unknown resource.";
+        }
+    } else {
+        (async () => {
+            const permissions = await browser.permissions.getAll();
+            if (permissions.origins.indexOf("*://gterminal.is-a.dev/*")) {
+                window.location.href = browser.runtime.getURL("/views/install/index.html");
+            } else {
+                const iframe = document.createElement("iframe");
+                iframe.src = "https://gterminal.is-a.dev" + page;
+                document.body.appendChild(iframe);
+            };
+        })();
+    }
 });
