36ddfc96d91d01d14be0811104c764d9b1e8dca5
Restyled.io <commits@restyled.io>
2023-08-31 22:46:13 +0000

    Restyled by clang-format

--- modifications.js
+++ modifications.js
@@ -1,37 +1,34 @@
-const match = /^https?:\/\/gterminal\.is-a\.dev(\/(index\.html|(\.github|modules)\/?.*|)|)((\?|#).*)?$/gm;
+const match =
+    /^https?:\/\/gterminal\.is-a\.dev(\/(index\.html|(\.github|modules)\/?.*|)|)((\?|#).*)?$/gm;
 
 document.querySelectorAll("a").forEach((element) => {
-    console.log(element.href, Boolean(match.exec(element.href)), match.exec(element.href))
-    if (!match.exec(element.href)) {
-        element.onclick = (event) => {
-            window.top.postMessage({
-                open: element.href
-            }, "*");
-            event.preventDefault();
-        }
+  console.log(element.href, Boolean(match.exec(element.href)),
+              match.exec(element.href))
+  if (!match.exec(element.href)) {
+    element.onclick = (event) => {
+      window.top.postMessage({open : element.href}, "*");
+      event.preventDefault();
     }
+  }
 })
 
 if (typeof gterminal != "undefined") {
-    gterminal.web.goto = (href) => {
-        window.top.postMessage({
-            open: href
-        }, "*");
-    }
+  gterminal.web.goto = (href) => { window.top.postMessage({open : href}, "*"); }
 
-    gterminal.title.set = (title) => {
-        window.top.postMessage({
-            set_title: title,
-            no_content: document.querySelector("#title").innerText == ""
-        }, "*");
-    };
+                                 gterminal.title.set = (title) => {
+    window.top.postMessage({
+      set_title : title,
+      no_content : document.querySelector("#title").innerText == ""
+    },
+                           "*");
+  };
 
-    if (gterminal.api_version != "1.3") {
-        window.top.postMessage({
-            set_title: "Version Mismatch"
-        }, "*");
-        window.location.href = "https://gterminal.is-a.dev/.github/notice/extension/VERSION_MISMATCH";
-    }
-} else {
-    console.warn("No gTerminal Instance!");
+  if (gterminal.api_version != "1.3") {
+    window.top.postMessage({set_title : "Version Mismatch"}, "*");
+    window.location.href =
+        "https://gterminal.is-a.dev/.github/notice/extension/VERSION_MISMATCH";
+  }
+}
+else {
+  console.warn("No gTerminal Instance!");
 }
--- views/terminal/script.js
+++ views/terminal/script.js
@@ -3,45 +3,44 @@ const page = (window.location.hash.substring(1) || "/");
 let platform = "unknown";
 
 if (typeof browser == "undefined" && typeof chrome != "undefined") {
-    var browser = chrome;
-    platform = "chrome";
+  var browser = chrome;
+  platform = "chrome";
 };
 
 window.onmessage = (event) => {
-    if (event.data.open) {
-        window.location.href = event.data.open;
+  if (event.data.open) {
+    window.location.href = event.data.open;
+  } else if (event.data.set_title) {
+    if (platform == "chrome" && event.data.no_content) {
+      document.querySelector("title").innerText = "> " + event.data.set_title;
+    } else {
+      document.querySelector("title").innerText = event.data.set_title;
     }
-    else if (event.data.set_title) {
-        if (platform == "chrome" && event.data.no_content) {
-            document.querySelector("title").innerText = "> " + event.data.set_title;
-        } else {
-            document.querySelector("title").innerText = event.data.set_title;
-        }
-    }
+  }
 };
 
-
 document.addEventListener("DOMContentLoaded", () => {
-
-    if (page.startsWith("/modules/")) {
-        document.body.innerText = "In the future, here should be an install helper. For now, I can only say:\n";
-        if (page.endsWith(".js")) {
-            document.body.innerText += "This is a module resource.";
-        } else if (page.endsWith(".json")) {
-            document.body.innerText += "This is a repo resource.";
-        } else {
-            document.body.innerText += "This is an unknown resource.";
-        }
+  if (page.startsWith("/modules/")) {
+    document.body.innerText =
+        "In the future, here should be an install helper. For now, I can only say:\n";
+    if (page.endsWith(".js")) {
+      document.body.innerText += "This is a module resource.";
+    } else if (page.endsWith(".json")) {
+      document.body.innerText += "This is a repo resource.";
     } else {
-        (async () => {
-            const permissions = await browser.permissions.getAll();
-            if (permissions.origins.indexOf("*://gterminal.is-a.dev/*")) {
-                window.location.href = browser.runtime.getURL("/views/install/index.html");
-            } else {
-                const iframe = document.createElement("iframe");
-                iframe.src = "https://gterminal.is-a.dev" + page;
-                document.body.appendChild(iframe);
-            };
-        })();
+      document.body.innerText += "This is an unknown resource.";
     }
+  } else {
+    (async () => {
+      const permissions = await browser.permissions.getAll();
+      if (permissions.origins.indexOf("*://gterminal.is-a.dev/*")) {
+        window.location.href =
+            browser.runtime.getURL("/views/install/index.html");
+      } else {
+        const iframe = document.createElement("iframe");
+        iframe.src = "https://gterminal.is-a.dev" + page;
+        document.body.appendChild(iframe);
+      };
+    })();
+  }
 });
